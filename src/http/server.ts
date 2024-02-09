1dafcd1ef1674e350855748ae2cce486dee1def1
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2024-02-08 22:00:40 -0300

    feat: add vote on poll endpoint route

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,7 @@ import cookie from '@fastify/cookie'
 
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
 
 const app = fastify()
 app.register(cookie, {
@@ -12,6 +13,7 @@ app.register(cookie, {
 
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server is running! 🚀')
