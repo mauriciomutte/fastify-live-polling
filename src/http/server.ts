4b77679aeddcd95c88cd316f83de8900eb5622c3
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2024-02-08 21:59:02 -0300

    chore: add fastify cookie

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,14 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 
 const app = fastify()
+app.register(cookie, {
+  secret: 'super-hard-secret-trust-me',
+  hook: 'onRequest',
+})
 
 app.register(createPoll)
 app.register(getPoll)
