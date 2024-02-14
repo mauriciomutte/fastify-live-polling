86e191f4bfc7ec4c1f74d9d4c0c60e3e9ffaf899
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2024-02-13 21:25:13 -0300

    feat: add poll results websocket route

--- src/http/server.ts
+++ src/http/server.ts
@@ -5,16 +5,21 @@ import websocket from '@fastify/websocket'
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import { pollResults } from './ws/poll-results'
 
 const app = fastify()
+// Plugins
+app.register(websocket)
 app.register(cookie, {
   secret: 'super-hard-secret-trust-me',
   hook: 'onRequest',
 })
-
+// HTTP routes
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+// WS routes
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server is running! 🚀')
